8ff0911d6c2cac3b1a0ab269695e3fb4e9325469
Hsiao <ehsiao@kih.kmart.com>
2016-11-08 13:04:30 -0600

    added function to pause gifs

--- assets/javascript/giphyWithPause.js
+++ assets/javascript/giphyWithPause.js
@@ -1,38 +1,44 @@
 $(document).ready();
 
-//Variables=============================
+
+//Objectives
 
 var searchTermArray = ['Elephants', 'Monkeys', 'Lions', 'Tigers', 'Polar Bears'];
+
 
 
 //Functions==================
 
 function renderButtons(){ 
 
-		// Deletes buttons prior to adding new buttons
+		// Deletes the movies prior to adding new movies (this is necessary otherwise you will have repeat buttons)
 		$('#buttons-view').empty();
 
-		// Loops through the array of search terms
+		// Loops through the array of movies
 		for (var i = 0; i < searchTermArray.length; i++){
 
-			// Then dynamicaly generates buttons for each search term in the array
-		    var a = $('<button>') // create the beginning and end tag. (<button></button>)
-		    a.addClass('searchTerm'); // Added a class for the search term buttons
-		    a.attr('data-name', searchTermArray[i]); // Added a data-name attribute to the button
+			// Then dynamicaly generates buttons for each movie in the array
+
+			// Note the jQUery syntax here... 
+		    var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
+		    a.addClass('searchTerm'); // Added a class 
+		    a.attr('data-name', searchTermArray[i]); // Added a data-attribute
 		    a.text(searchTermArray[i]); // Provided the initial button text
 		    $('#buttons-view').append(a); // Added the button to the HTML
 		}
 	}
 
-// Function for rendering the JSON content results from API for each button into the div
+
+
+// Generic function for dumping the JSON content for each button into the div
 	function displayGif(){
 
-		var searchTerm = $(this).attr('data-name'); 
+		var searchTerm = $(this).attr('data-name'); //button.name
 		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-		//Hit the queryURL, take the data and display it in the div with an id of gallery
+		//Write code between the dashes below to hit the queryURL, take the data and display it in the div with an id of moviesView
 		$('#gallery').empty();	
-		
+		//------YOUR CODE GOES IN THESE DASHES
 		$.ajax({
 				url: queryURL, 
 				method: 'GET'
@@ -54,14 +60,15 @@ function renderButtons(){
 					'data-state': "animate",
 				});
 
-				gifDiv.append(p);
-				gifDiv.append(image);
+				gifDiv.append(p)
+				gifDiv.append(image)
 
-				$('#gallery').prepend(gifDiv);
-				$('#galleryTitle').html("Giffy Gallery - " + results.length + " Results");
+				$('#gallery').prepend(gifDiv)
 			}
 		});
+		//------
 	}
+
 
 	function pauseGif() {
             var state = $(this).attr('data-state');
@@ -70,10 +77,12 @@ function renderButtons(){
                 $(this).attr('src', $(this).data('still'));
                 $(this).attr('data-state', 'still');
             }
+
             else{
                 $(this).attr('src', $(this).data('animate'));
                 $(this).attr('data-state', 'animate');
-            }           
+            }
+            
     }
 
 //Buttons/Images============================================
@@ -91,15 +100,21 @@ $('#submitTerm').on('click', function(){
 		// clears the input field after clicking submit
 		$('input[type="text"]').val('');
 
-		// The search terms entered by the user from the textbox is added to the existing search Term array
+		// The movie from the textbox is then added to our array
 		searchTermArray.push(searchTerm);
 		
-		// Runs function to render buttons based on the new results of the array
+
+		// Our array then runs which handles the processing of our movie array
 		renderButtons();
 
-		// This line is so that users can hit "enter" instead of clicking on the submit button and it won't move to the next page
+
+
+		// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
 		return false;
 	})
+
+
+
 
 
 // ========================================================
